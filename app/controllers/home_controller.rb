138603818f5aefaf73979f6c3d3e2e1318cac8f8
Divij Jain <jaindivijwork@gmail.com>
2022-01-06 23:38:11 +0530

    Finished the project

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :about]
+
   def index
   end
   
   def about
+    @about_me = "My name is Divij Jain!"
   end
 end
